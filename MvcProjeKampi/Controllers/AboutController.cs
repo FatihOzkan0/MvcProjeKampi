98e0eac3a7e90a69d9d822fcacd20668ac192f9c
FATIH <FATIH@DESKTOP-TJ8KD78>
2023-06-16 12:16:03 +0300

    Yeni çalışmalar.

--- MvcProjeKampi/Controllers/AboutController.cs
+++ MvcProjeKampi/Controllers/AboutController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +11,28 @@ namespace MvcProjeKampi.Controllers
 {
     public class AboutController : Controller
     {
-        // GET: About
+        AboutManager am = new AboutManager(new EfAboutDal());
         public ActionResult Index()
         {
+            var aboutValues = am.GetList();
+            return View(aboutValues);
+        }
+
+        [HttpGet]
+        public ActionResult AddAbout()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult AddAbout(About p) 
+        {
+            am.AboutAdd(p);
+            return RedirectToAction("Index");
+        }
+
+        public PartialViewResult AboutPartial()        //Partial kullanmamızın nedeni Index methodunun içinde hem ekleme hem listeleme işlemi yapmamız.
+        {
+            return PartialView();
         }
     }
 }
