3ef1187b10f54f1c87c7588744c04e05fa18f713
FATIH <fthokn57@icloud.com>
2023-07-10 13:18:43 +0300

    Writer Login the writer home page has been set up.

--- MvcProjeKampi/Controllers/LoginController.cs
+++ MvcProjeKampi/Controllers/LoginController.cs
@@ -21,18 +21,43 @@ namespace MvcProjeKampi.Controllers
         public ActionResult Index(Admin p)
         {
             Context context = new Context();
-            var adminUserInfo = context.Admins.FirstOrDefault(x=>x.AdminUserName==p.AdminUserName && x.AdminPassword==p.AdminPassword);
-            if (adminUserInfo!=null)
+            var adminUserInfo = context.Admins.FirstOrDefault(x => x.AdminUserName == p.AdminUserName && x.AdminPassword == p.AdminPassword);
+            if (adminUserInfo != null)
             {
                 FormsAuthentication.SetAuthCookie(adminUserInfo.AdminUserName, false);       //Yetkilendirme işlemini yapıyoruz ve UserName yetki veriyoruz
                 Session["AdminUserName"] = adminUserInfo.AdminUserName;
-                return RedirectToAction("Index","AdminCategory");
+                return RedirectToAction("Index", "AdminCategory");
             }
             else
             {
                 return RedirectToAction("Index");
             }
+
+
+        }
+        //Yazar Giriş Sayfası
+
+        [HttpGet]
+        public ActionResult WriterLogin()
+        {
             return View();
+        }
+        [HttpPost]
+        public ActionResult WriterLogin(Writer p)
+        {
+            Context context = new Context();
+            var writerUserInfo = context.Writers.FirstOrDefault(x => x.WriteMail == p.WriteMail && x.WriterPassword == p.WriterPassword);
+            if (writerUserInfo != null)
+            {
+                FormsAuthentication.SetAuthCookie(writerUserInfo.WriteMail, false);       //Yetkilendirme işlemini yapıyoruz ve UserName yetki veriyoruz
+                Session["AdminUserName"] = writerUserInfo.WriteMail;
+                return RedirectToAction("MyContent", "WriterPanelContent");
+            }
+            else
+            {
+                return RedirectToAction("WriterLogin");
+            }
+
         }
     }
 }
